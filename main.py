f5613f2518fa2d7c9e573ece2eaa464dc58681d6
Luka Jovanovic <luka@youshitsune.me>
2022-09-17 18:42:06 +0200

    Set metric system

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ st.write("""
         # Weather report
         """)
 location = st.text_input("Type in your location")
-response = requests.get(f"https://wttr.in/{location}?format='%l:+%c+%t'")
+response = requests.get(f"https://wttr.in/{location}?format='%l:+%c+%t'?m")
 st.write(response.text)
