7d6a2b8503fa534c02ba91cbcb7681fade4954ae
Luka Jovanovic <luka@youshitsune.me>
2022-10-11 16:15:29 +0200

    Cleaning up

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 import streamlit as st
 
-st.write("""
-        # Weather report
-        """)
+st.write("# Weather report")
 location = st.text_input("Type in your location")
 response = requests.get(f"https://wttr.in/{location}?format='%l:+%c+%t'&m")
 st.write(response.text)
